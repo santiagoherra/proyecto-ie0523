66e72dbc5915307234b8f573239cdd758743172b
LucasDM1 <lucas19651@hotmail.com>
2024-06-18 22:21:37 -0600

    MDIO casi terminado

--- MDIO.v
+++ MDIO.v
@@ -22,7 +22,12 @@ module MDIO (
     output [5:0] counter
 );
 
-reg [6:0] count;
+reg [5:0] count;
+reg mdio_start_d; 
+reg [1:0] state;
+
+
+
 
 always @(posedge clk) begin
     if(!reset) begin
@@ -32,16 +37,37 @@ always @(posedge clk) begin
         RD_DATA <= 0;
         DATA_RDY <= 0;
         count <= 6'd32;
+        mdio_start_d <=0;
     end
     else begin 
-        if (MDIO_START) begin
+        
+        MDC <= ~MDC;//cada posedge se cambia el estado de MDC para dividir a la mitad la frecuencia
+        if(MDIO_START) begin
+            mdio_start_d <= MDIO_START;//Señal complementaría para poder usarla para iniciar la transmisión
+        end
+        
+        if(mdio_start_d) begin
+            MDIO_OE <= 1;// Si mdio_start_1 significa que hay una escritura por lo que se pone en uno
             if(count > 0) begin
                 MDIO_OUT <= T_DATA[count-1]; 
                 count <= count -1;
             end else begin
-                DATA_RDY <= 1;
-                MDIO_OE <= 0;
+                mdio_start_d = 0;
+                MDIO_OE <= 0; //Se pone en 0 cuando terminan los 32 ciclos
+                count <= 6'd32; //Se reincia el contador
             end
+        end else begin 
+            MDIO_OE <= 1;
+            if(count > 0) begin
+                if(count < 16) begin //cuando sea menor a 16 se baja
+                    MDIO_OE <= 0;
+                    RD_DATA <= {RD_DATA[14:0], MDIO_IN}; //Se concatena el bit de MDIO_IN a los 15 bits menos significativos, 
+                    count <= count -1; //y asi generar 16 bits, y se repite hasta haber creado la palabra de 16 bits
+                end
+            end else begin
+                DATA_RDY <= 1;
+            end
+            
         end
     end
     
--- MDIO_tester.v
+++ MDIO_tester.v
@@ -11,7 +11,7 @@ module tester (
     input MDIO_OE,
     input [15:0] RD_DATA,
     input DATA_RDY,
-    input [4:0] counter
+    input [5:0] counter
 );
 
 always begin
@@ -26,14 +26,14 @@ initial begin
 
 end
 initial begin
+    //Caso de Escritura
     #10
     reset =1;
-    T_DATA = 32'hAAAAAAAA;
+    T_DATA = 32'h8C6AF5B5; 
     #10 
     MDIO_START = 1;
     #10
-    // MDIO_START = 0;
-    //wait(DATA_RDY)
+    MDIO_START = 0;
     #400
     $finish;  
 end
